971d8cc2c392f4374893ebb703934f7db3c987e6
kavindyakdn <kavindya.kdn@gmail.com>
2025-10-28 18:44:23 +0530

    Connecting MongoDB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DevicesModule } from './devices/devices.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // loads .env
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }), DevicesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
